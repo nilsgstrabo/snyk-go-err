fd75593fc79309fb1b486661651f289da69624f7
Nils Gustav Stråbø <nst@equinor.com>
2023-03-27 13:01:04 +0200

    csi 1.3.2

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 
 	"github.com/equinor/radix-common/utils/slice"
 	// "github.com/urfave/negroni/v3"
-	// secretproviderfake "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned/fake"
+	secretproviderfake "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned/fake"
 )
 
 func main() {
 	// negroni.New()
 	fmt.Println("done")
 	slice.All(nil, func(s string) bool { return true })
-	// secretproviderfake.NewSimpleClientset()
+	secretproviderfake.NewSimpleClientset()
 }
