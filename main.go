4a54917e1c778dcc35c17affc7d1a068f4197036
Nils Gustav Stråbø <nst@equinor.com>
2023-03-27 12:59:28 +0200

    test

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/urfave/negroni/v3"
-	secretproviderfake "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned/fake"
+	"github.com/equinor/radix-common/utils/slice"
+	// "github.com/urfave/negroni/v3"
+	// secretproviderfake "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned/fake"
 )
 
 func main() {
-	negroni.New()
+	// negroni.New()
 	fmt.Println("done")
-	secretproviderfake.NewSimpleClientset()
+	slice.All(nil, func(s string) bool { return true })
+	// secretproviderfake.NewSimpleClientset()
 }
