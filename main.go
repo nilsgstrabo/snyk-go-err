601720a2b5801bff986bd441bd361c0f77f84947
Nils Gustav Stråbø <nst@equinor.com>
2023-03-27 12:51:21 +0200

    add csi 1.1.0

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/urfave/negroni/v3"
+	secretproviderfake "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned/fake"
 )
 
 func main() {
 	negroni.New()
 	fmt.Println("done")
+	secretproviderfake.NewSimpleClientset()
 }
